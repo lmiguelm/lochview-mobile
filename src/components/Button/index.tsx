22988166476a0dc31d2192ff43f1ebbd785f70e1
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-09-23 23:22:17 -0300

    Melhorando componente de botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { RectButtonProps } from 'react-native-gesture-handler';
+import { useTheme } from 'styled-components';
 
 import { Container, Title } from './styles';
 
@@ -7,12 +9,31 @@ type Props = RectButtonProps & {
   title: string;
   background?: string;
   light?: boolean;
+  loading?: boolean;
 };
 
-export function Button({ title, background, light = false, ...rest }: Props) {
+export function Button({
+  title,
+  background,
+  light = false,
+  loading = false,
+  enabled = true,
+  ...rest
+}: Props) {
+  const { colors } = useTheme();
+
   return (
-    <Container {...rest} background={background}>
-      <Title light={light}>{title}</Title>
+    <Container
+      {...rest}
+      background={background}
+      enabled={!loading && enabled}
+      disabled={loading || !enabled}
+    >
+      {loading ? (
+        <ActivityIndicator color={colors.light} size="small" />
+      ) : (
+        <Title light={light}>{title}</Title>
+      )}
     </Container>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,9 +1,10 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 type ButtonProps = {
   background?: string;
+  disabled?: boolean;
 };
 
 type TitleProps = {
@@ -12,19 +13,25 @@ type TitleProps = {
 
 export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
-  height: 70px;
+  height: 56px;
 
   justify-content: center;
   align-items: center;
 
   margin-bottom: 10px;
 
-  border-radius: 8px;
+  border-radius: 5px;
   background-color: ${({ theme, background }) => (background ? background : theme.colors.primary)};
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      opacity: 0.7;
+    `}
 `;
 
 export const Title = styled.Text<TitleProps>`
   color: ${({ theme, light }) => (light ? theme.colors.title : theme.colors.light)};
   font-family: ${({ theme }) => theme.fonts.medium};
-  font-size: ${RFValue(24)}px;
+  font-size: ${RFValue(15)}px;
 `;
